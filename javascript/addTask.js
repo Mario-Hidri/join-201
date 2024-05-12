0a48b4097ac3c0b406126ece8f8931cc271f68ba
Fabian-85 <fabian.maier0805@hotmail.com>
2024-05-12 16:08:33 +0200

    add subtask icon in add_task.html

---
+++ javascript/addTask.js
@@ -0,0 +1,14 @@
+let task = [];
+
+function addTask(){
+
+    let titel = document.getElementById(titel);
+    let description = document.getElementById(description);
+    let date = document.getElementById(date);
+    let priority;
+    let category = document.getElementById(date);
+   // let subtask = document.getElementById();
+
+
+
+}
