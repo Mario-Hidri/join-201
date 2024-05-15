8d9a05d72a7a0cc8100775554f95d0c7266d3e02
Mario Hidri <mariohidri2014@yahoo.com>
2024-05-15 17:59:19 +0200

    save contact to local storage

--- javascript/add_contact.js
+++ javascript/add_contact.js
@@ -1,8 +1,69 @@
+let allContacts = [];
+
+init();
+
+
+function addContact() {
+    let name = document.getElementById('add_contact_name').value;
+    let email = document.getElementById('add_contact_email').value;
+    let phone = document.getElementById('add_contact_phone').value;
+
+    let contact = {
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'createdAt': new Date().getTime()
+    };
+
+    allContacts.push(contact);
+
+    let allContactsAsString = JSON.stringify(allContacts);
+    localStorage.setItem('allContacts', allContactsAsString);
+}
+
+function init() {
+    includeHTML();
+
+    let allContactsAsString = localStorage.getItem('allContacts');
+    if (allContactsAsString) {
+        allContacts = JSON.parse(allContactsAsString) || [];
+    } else {
+        allContacts = [];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function hidePage() {
     let slidingPage = document.getElementById('slidingPage');
     slidingPage.classList.remove('show');
     slidingPage.classList.add('hide');
-    
+
     setTimeout(() => {
         slidingPage.classList.add('hidden');
     }, 300);
@@ -12,7 +73,4 @@ function showPage() {
     const slidingPage = document.getElementById('slidingPage');
     slidingPage.classList.remove('hidden', 'hide');
     slidingPage.classList.add('show');
-
-    
 }
-
