6f4af45c18657f20464cb67e1352646decdb44cc
Mario Hidri <mariohidri2014@yahoo.com>
2024-05-16 21:32:18 +0200

    update

--- javascript/add_contact.js
+++ javascript/add_contact.js
@@ -1,7 +1,27 @@
+let defaultContacts = [
+    {
+        name: "Anton Mayer",
+        email: "antonm@gmail.com",
+        phone: "444"
+    },
+    {
+        name: "Anton Mayer",
+        email: "antonm@gmail.com",
+        phone: "444"
+    },
+    {
+        name: "Anton Mayer",
+        email: "antonm@gmail.com",
+        phone: "444"
+    },
+    {
+        name: "Anton Mayer",
+        email: "antonm@gmail.com",
+        phone: "444"
+    }
+];
+
 let allContacts = [];
-
-init();
-
 
 function addContact() {
     let name = document.getElementById('add_contact_name').value;
@@ -9,16 +29,18 @@ function addContact() {
     let phone = document.getElementById('add_contact_phone').value;
 
     let contact = {
-        'name': name,
-        'email': email,
-        'phone': phone,
-        'createdAt': new Date().getTime()
+        name: name,
+        email: email,
+        phone: phone,
+        createdAt: new Date().getTime()
     };
 
     allContacts.push(contact);
 
     let allContactsAsString = JSON.stringify(allContacts);
     localStorage.setItem('allContacts', allContactsAsString);
+
+    renderContacts();
 }
 
 function init() {
@@ -28,36 +50,12 @@ function init() {
     if (allContactsAsString) {
         allContacts = JSON.parse(allContactsAsString) || [];
     } else {
-        allContacts = [];
+        // If no contacts are in localStorage, use the default contacts
+        allContacts = defaultContacts;
     }
+
+    renderContacts();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function hidePage() {
     let slidingPage = document.getElementById('slidingPage');
@@ -74,3 +72,34 @@ function showPage() {
     slidingPage.classList.remove('hidden', 'hide');
     slidingPage.classList.add('show');
 }
+
+function renderContacts() {
+    let contacts_container = '';
+
+    for (let i = 0; i < allContacts.length; i++) {
+        contacts_container += generateContactHTML(allContacts[i]);
+    }
+    document.getElementById('contacts_container_A').innerHTML = contacts_container;
+}
+
+function generateContactHTML(contact) {
+    const lastNameInitial = contact.name.split(' ')[1]?.charAt(0) || '';
+
+    return `
+        <div class="contact_card">
+            <div class="image_container">
+                <img src="./assets/img/contacts_img/Ellipse 5.png" alt="Profile Picture">
+                <div class="initials">
+                    <span class="initials1">${contact.name.charAt(0)}</span>
+                    <span class="initials2">${lastNameInitial}</span>
+                </div>
+            </div>
+            <div class="contact_info">
+                <h1>${contact.name}</h1>
+                <a href="mailto:${contact.email}">${contact.email}</a>
+            </div>
+        </div>
+    `;
+}
+
+window.onload = init;
