378a55dcc8065746ded825bbafe8810f463d9a1e
Mario Hidri <mariohidri2014@yahoo.com>
2024-06-12 18:56:21 +0200

    privacy and legal notice responsive

--- javascript/generatedHTML_add_contact.js
+++ javascript/generatedHTML_add_contact.js
@@ -1,11 +1,26 @@
+/**
+ * An array of predefined colors.
+ * @type {string[]}
+ */
 
 const colors = ['#FF6F61', '#6B8E23', '#4682B4', '#DA70D6', '#FF6347', '#40E0D0', '#FFD700', '#FFA07A', '#7FFFD4', '#BA55D3', '#6495ED', '#8A2BE2', '#FF4500', '#00CED1', '#B0E0E6', '#8B008B', '#20B2AA', '#9932CC', '#FF1493', '#00FF7F', '#FF7F50', '#4682B4', '#9370DB', '#D2691E', '#32CD32', '#8B4513', '#00FFFF', '#8FBC8F', '#FF69B4', '#00BFFF', '#FFA500', '#006400', '#8B008B', '#2E8B57', '#B22222', '#00FA9A'];
 
+/**
+ * Returns a random color from the predefined list of colors.
+ * @returns {string} A random color from the `colors` array.
+ */
 function getRandomColor() {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
 
+/**
+ * Generates the HTML content for a contact card.
+ * @param {Object} contact - The contact object.
+ * @param {number} index - The index of the contact in the contact list.
+ * @param {string} randomColor - The random color assigned to the contact.
+ * @returns {string} The HTML string for the contact card.
+ */
 function generateContactHTML(contact, index, randomColor) {
     const lastNameInitial = contact.name.split(' ')[1]?.charAt(0) || '';
 
@@ -25,6 +40,11 @@ function generateContactHTML(contact, index, randomColor) {
     `;
 }
 
+/**
+ * Creates the content for a contact card.
+ * @param {Object} contact - The contact object.
+ * @returns {string} The HTML string for the content of the contact card.
+ */
 function createContactCardContent(contact) {
     return `
         <div class="image_container">
@@ -36,6 +56,12 @@ function createContactCardContent(contact) {
         </div>`;
 }
 
+/**
+ * Generates the initials for a contact's name.
+ * @param {string} firstNameInitial - The initial of the contact's first name.
+ * @param {string} lastNameInitial - The initial of the contact's last name.
+ * @returns {string} The HTML string for the initials.
+ */
 function generateInitials(firstNameInitial, lastNameInitial) {
     return `
         <div class="initials_edit">
